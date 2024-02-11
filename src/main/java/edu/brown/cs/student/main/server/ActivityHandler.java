d01aa59c89ae4a9e0bea14d49c121e16d51b64b6
Isabella Delionado <isabella_delionado@brown.edu>
2024-02-10 23:01:46 -0500

    chore: outlined methods needed for ActivityHandler

--- src/main/java/edu/brown/cs/student/main/server/ActivityHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/ActivityHandler.java
@@ -90,4 +90,26 @@ public class ActivityHandler implements Route {
 
     return sentBoredApiResponse.body();
   }
+
+  private loadCSV{
+    //load csv file if one is located at specific path
+    //requests must have filepath query parameter
+    //response must include filepath field
+    //share status of file loading with search and view
+  }
+
+  private viewCSV{
+    //sends back entire CSV file's conentnts as a json 2d array
+  }
+
+  private searchCSV{
+    //send back rows mathcing the given search criteria
+    //requests muts allow all search paramters needed to implement CSV
+  }
+
+  private broadband{
+    //sends back broadband data from ACS
+    //requests must have state & county query parameter
+    //response must include correspondng fields for all paramters in request recieved
+  }
 }
