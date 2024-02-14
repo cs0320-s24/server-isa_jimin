d51d20e53c9bf21824c66e640356684b65e44454
Isabella Delionado <isabella_delionado@brown.edu>
2024-02-14 14:46:44 -0500

    fix: resolved some issues in proxy class

--- src/main/java/edu/brown/cs/student/main/server/backend/Proxy.java
+++ src/main/java/edu/brown/cs/student/main/server/backend/Proxy.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 import java.security.Key;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 //need to decide when/how to remove stale (outdated or irrelevant)
@@ -13,34 +14,39 @@ import java.util.concurrent.TimeUnit;
 //class could assist in managing cached data and
 //implementing configurable eviction policies
 public class Proxy<Graph> extends Ticker {
-
     public  Proxy(){
-        LoadingCache<Key, Graph> graphs = CacheBuilder.newBuilder().ticker(Ticker);
-                //.expireAfterAccess(long,TimeUnit);
-
-                //CacheBuilder.ticker(Ticker);
-                //CacheBuilder.newBuilder().maximumWeight(100000).weigher(new Weigher<Key, Graph>() {
-//            public int weigh(Key k, Graph g) {return g.vertices().size();}}).build(new CacheLoader<Key, Graph>() {
-//                public Graph load(Key key) { // no checked exception
-//                 return createExpensiveGraph(key);}
-//            });
-
-        CacheLoader<Key, DatabaseConnection> loader = new CacheLoader<Key, DatabaseConnection> () {
-            public DatabaseConnection load(Key key) throws Exception {
-                return openConnection(key);
-            }
-        };
+//        CacheLoader<Key, DatabaseConnection> loader = new CacheLoader<Key, DatabaseConnection> () {
+//            public DatabaseConnection load(Key key) throws Exception {
+//                return openConnection(key);
+//            }
+//        };
         RemovalListener<Key, DatabaseConnection> removalListener = new RemovalListener<Key, DatabaseConnection>() {
             public void onRemoval(RemovalNotification<Key, DatabaseConnection> removal) {
                 DatabaseConnection conn = removal.getValue();
                 conn.close(); // tear down properly
             }
         };
-
-        return CacheBuilder.newBuilder()
-                .expireAfterAccess(2, TimeUnit.MINUTES)
+        LoadingCache<Key, Graph> graphs = CacheBuilder.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
                 .removalListener(removalListener)
-                .build(loader);
+                .build(
+                        new CacheLoader<>() {
+                            @Override
+                            public Graph load(Key key) throws Exception {
+                                return createExpensiveGraph(key);
+                            }
+                        });
+//        CacheBuilder<Object, Object> graphs = CacheBuilder.newBuilder()
+//             //   .ticker(Ticker)
+//                .expireAfterAccess(2,TimeUnit.MINUTES);
+
+                        //CacheBuilder.ticker(Ticker);
+                        //CacheBuilder.newBuilder().maximumWeight(100000).weigher(new Weigher<Key, Graph>() {
+//            public int weigh(Key k, Graph g) {return g.vertices().size();}}).build(new CacheLoader<Key, Graph>() {
+//                public Graph load(Key key) { // no checked exception
+//                 return createExpensiveGraph(key);}
+//            });
     }
     @Override
     public long read() {
