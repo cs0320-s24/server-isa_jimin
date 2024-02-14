ba8aaa3e6d438c1e9150add895b890a1ea78f0cb
Jimin Ryu <jimin_ryu@brown.edu>
2024-02-13 21:05:52 -0500

    Implemented handler response request features

--- src/main/java/edu/brown/cs/student/main/server/CSV/LoadCsv.java
+++ src/main/java/edu/brown/cs/student/main/server/CSV/LoadCsv.java
@@ -1,5 +1,8 @@
 package edu.brown.cs.student.main.server.CSV;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import edu.brown.cs.student.main.server.backend.handler.OrderHandler;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -7,6 +10,7 @@ import spark.Route;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class LoadCsv implements Route {
 
@@ -21,9 +25,42 @@ public class LoadCsv implements Route {
         FileReader fileReader = new FileReader(filePath);
         //BufferedReader reader = new BufferedReader(fileReader);
         try(fileReader){
-            return "File loaded successfully";
+            return LoadValidResponse("success", ).serialize();
         }catch(IOException e){
-            return "Error loading file";
+            return LoadInvalidResponse("failure", ).serialize();
         }
-}}
+
+    }
+
+    public record LoadValidResponse(String response, Map<String, Object> responseMap) {
+        /**
+         * @return this response, serialized as Json
+         */
+        String serialize() {
+            try {
+                // Initialize Moshi which takes in this class and returns it as JSON!
+                Moshi moshi = new Moshi.Builder().build();
+                JsonAdapter<OrderHandler.SoupSuccessResponse> adapter = moshi.adapter(OrderHandler.SoupSuccessResponse.class);
+                return adapter.toJson();
+            } catch (Exception e) {
+                //should return a type error, added with e.getMessage()
+            }
+        }
+    }
+
+    /** Response object to send if someone requested soup from an empty Menu */
+    public record LoadInvalidResponse(String response_type, ) {
+        public LoadInvalidResponse(String response, Map<String, Object> responseMap) {
+            this("error");
+        }
+
+        /**
+         * @return this response, serialized as Json
+         */
+        String serialize() {
+            Moshi moshi = new Moshi.Builder().build();
+            return moshi.adapter(OrderHandler.SoupNoRecipesFailureResponse.class).toJson(this);
+        }
+    }
+}
 
