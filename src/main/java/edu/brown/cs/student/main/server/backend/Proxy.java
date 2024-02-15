908a52381080813bb8548417ebe9cdd27a989735
Isabella Delionado <isabella_delionado@brown.edu>
2024-02-15 14:43:51 -0500

    fix: made more edits to proxy class- need to figure out how to associate request and result

--- src/main/java/edu/brown/cs/student/main/server/backend/Proxy.java
+++ src/main/java/edu/brown/cs/student/main/server/backend/Proxy.java
@@ -1,7 +1,6 @@
 package edu.brown.cs.student.main.server.backend;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.common.cache.*;
+import edu.brown.cs.student.main.server.backend.handler.CensusHandler;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -17,66 +16,37 @@ import java.util.concurrent.TimeUnit;
 
 //public class Proxy<Graph> extends Ticker{
 public class Proxy implements Route{
-   private  Route wrappedRoute;
-   private final LoadingCache<String, Collection<String>> cache;
-    public  Proxy(){
-        LoadingCache<String, Collection<String>> cache1;
+   private final CensusHandler wrappedHandler;
+   private final LoadingCache<String, Object> cache;
+    public  Proxy(CensusHandler wrappedHandler) {
+        this.wrappedHandler = wrappedHandler;
+        RemovalListener<String, Object> removalListener = removalNotification -> {
 
-//        CacheLoader<Key, DatabaseConnection> loader = new CacheLoader<Key, DatabaseConnection> () {
-//            public DatabaseConnection load(Key key) throws Exception {
-//                return openConnection(key);
-//            }
-//        };
+        };
 
         this.cache = CacheBuilder.newBuilder()
                 .maximumSize(10)
                 .expireAfterWrite(10, TimeUnit.MINUTES)
                 .recordStats()
+                .removalListener(removalListener)
                 .build(
-                        new CacheLoader<>(){
+                        new CacheLoader<>() {
                             @Override
                             public Collection<String> load(String s) throws Exception {
-                                return wrappedRoute.handle();
+                                return wrappedHandler.handle(request);
                             }
                             //load(request, response)
                         }
                 );
-
-
-//        RemovalListener<Key, DatabaseConnection> removalListener = new RemovalListener<Key, DatabaseConnection>() {
-//            public void onRemoval(RemovalNotification<Key, DatabaseConnection> removal) {
-//                DatabaseConnection conn = removal.getValue();
-//                conn.close(); // tear down properly
-//            }
-//        };
-//        LoadingCache<Key, Graph> graphs = CacheBuilder.newBuilder()
-//                .maximumSize(10000)
-//                .expireAfterWrite(10, TimeUnit.MINUTES)
-//                .recordStats()
-//                //.removalListener(removalListener)
-//                .build(
-//                        new CacheLoader<>() {
-//                            @Override
-//                            public Graph load(String key) throws Exception {
-//                                //return createExpensiveGraph(key);
-//                                return wrapped
-//                            }
-//                        });
-////        CacheBuilder<Object, Object> graphs = CacheBuilder.newBuilder()
-////             //   .ticker(Ticker)
-////                .expireAfterAccess(2,TimeUnit.MINUTES);
-//
-//                        //CacheBuilder.ticker(Ticker);
-//                        //CacheBuilder.newBuilder().maximumWeight(100000).weigher(new Weigher<Key, Graph>() {
-////            public int weigh(Key k, Graph g) {return g.vertices().size();}}).build(new CacheLoader<Key, Graph>() {
-////                public Graph load(Key key) { // no checked exception
-////                 return createExpensiveGraph(key);}
-////            });
-
     }
     @Override
-    public Collection<String> handle(Request request, Response response) throws Exception {
-        Collection<String> result = this.cache.getUnchecked(response.toString());
-        return result;
+    public Object handle(Request request, Response response) throws Exception {
+        String cacheKey = generateCacheKey(request);
+        return this.cache.getUnchecked(cacheKey);
+    }
+
+    private String generateCacheKey(Request request){
+        //figure out how to do this
+        return null;
     }
 }
